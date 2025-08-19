b3b0ea96d2e116c8d61620fd5f35a8c9857b462f
Guillaume Sere <guillaumesere@hotmail.fr>
2025-08-19 15:27:12 +0200

    modif css

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -5,18 +5,18 @@ const Description = () => {
         <div className='description'>
             <h1 className='title-description'>Description</h1>
             <div className="intro">
-                <p>🌱📱 Découvrez mon application de gestion des plantes,
+                <p className='text-description'>🌱📱 Découvrez mon application de gestion des plantes,
                     conçue pour simplifier et améliorer votre expérience en
                     tant qu'amateur de plantes !🌱📱 Découvrez mon application
                     de gestion des plantes, conçue pour simplifier et améliorer
                     votre expérience en tant qu'amateur de plantes !</p><br />
-                <p>🌿 Avec cette application, vous pouvez créer votre propre
+                <p className='text-description'>🌿 Avec cette application, vous pouvez créer votre propre
                     collection de plantes en ajoutant les informations
                     essentielles telles que le nom de la plante, l'espèce,
                     la quantité d'eau nécessaire et la fréquence d'arrosage.
                     Pour la fréquence d'arrosage, il faut indiquer le nombre de
                      jours entre lesquel l'arrosage doit être effectuer.</p><br />
-                <p>📅 Le calendrier intégré vous permet de planifier les dates
+                <p className='text-description'>📅 Le calendrier intégré vous permet de planifier les dates
                     d'arrosage pour chaque plante. Il vous suffit de sélectionner
                     une date dans le calendrier et l'application vous rappellera
                     automatiquement quand il est temps d'arroser vos plantes.
